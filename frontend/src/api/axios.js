cdb6cd1eabe937079f587924c749e780b505db3a
ronithdhanesh <ronithdhanesh@gmail.com>
2026-06-11 21:29:04 +0530

    add new backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "http://localhost:3000"
+    baseURL : "https://employee-app-backend-4vve.onrender.com"
 })
 
 api.interceptors.request.use(
