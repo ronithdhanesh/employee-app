a2fbc89c7127c81be74a9467721a105bafb5f288
ronithdhanesh <ronithdhanesh@gmail.com>
2026-06-12 10:57:17 +0530

    refresh  frontend update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://localhost:3000",
+    "https://employee-app-backend-4vve.onrender.com/",
 });
 
 api.interceptors.request.use(
